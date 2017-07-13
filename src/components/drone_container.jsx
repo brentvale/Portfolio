c2b10421657849c3498e565005051c5301ef5fb6
Brent Vale <brentvale@gmail.com>
2017-07-13 15:27:10 -0700

    finishes drone animation avoidance, refactors css into multiple files based on media queries

--- src/components/drone_container.jsx
+++ src/components/drone_container.jsx
@@ -1,12 +1,41 @@
 import React, {Component} from 'react';
 
 class DroneContainer extends Component{
+	constructor(){
+		super();
+		this.state = {
+			mouseHovered: false,
+			//need to make sure duration in miliseconds matches up with animation timing for two classes:
+			//.drone-container-hovering.avoid-left && .drone-container-hovering.avoid-right
+			animationInterval: 2000
+		}
+		this.handleDroneAvoid = this.handleDroneAvoid.bind(this);
+	}
+	
+	handleDroneAvoid(e){
+		setTimeout(() => {
+			this.setState({mouseHovered: false});
+		}, this.state.animationInterval)
+		this.setState({mouseHovered: true});
+	}
 	render(){
-		return(
-			<div className="drone-container center-block">
-				
-			</div>
-		);
+		let klass = "drone-container drone-container-hovering";
+		if(this.state.mouseHovered){
+			klass += (Math.random() < 0.50) ? " avoid-left" : " avoid-right";
+		} 
+
+		if(this.state.mouseHovered){
+			return(
+				<div className={klass}>
+				</div>
+			);
+		} else {
+			return(
+				<div className={klass} onMouseEnter={this.handleDroneAvoid} onTouchStart={this.handleDroneAvoid}>
+				</div>
+			);
+		}
+		
 	}
 }
 export default DroneContainer;
